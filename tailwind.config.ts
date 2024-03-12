164a4e7babe676e9b58b268f997cbbb3c8074e1f
AlejandroCristM <alejandro.cristanchom@gmail.com>
2024-03-11 21:51:16 -0500

    feat: adding next app and customize a little

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          600: "var(--primary-color)",
+        },
+        sky: {
+          200: "var(--color-sky-200)",
+          900: "var(--color-sky-900)",
+        },
+      },
     },
   },
   plugins: [],
